35db72b834b56bdbb8e7ea6cda4f61bb6c58c2c7
Lugmun <mangutovnr@mail.ru>
2020-12-07 22:38:36 +0300

    4

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -47,9 +47,6 @@ public class Client {
                 }
             }
 
-
-            //System.out.println("Можете начать работать");
-
         }catch (NoSuchElementException e){
             System.out.println("Конец ввода");
             System.exit(0);
@@ -61,11 +58,7 @@ public class Client {
                 new InetSocketAddress(this.add, this.port);
             try {
                 while (true) {
-                    //System.out.println("Клиент запущен");
-
-
                     SocketChannel socketChannel = SocketChannel.open();
-                    //socketChannel.configureBlocking(false);
                     boolean isConnected = socketChannel.connect(a);
                     if (isConnected) {
                         System.out.println("connected");
@@ -74,14 +67,10 @@ public class Client {
                         ByteBuffer forWrite = ByteBuffer.allocate(1024*1024);
                         forWrite = ByteBuffer.wrap(Serializer.serialize(terminal.start()));
                         forWrite.flip();
-                        //socketChannel.write(f);
+
                         socketChannel.socket().getOutputStream().write(forWrite.array());
                         socketChannel.socket().getOutputStream().flush();
-                        //socketChannel.socket().getOutputStream().close();
 
-
-
-                        //f.clear();
                         System.out.println(forWrite.clear());
 
                         ByteBuffer forRead = ByteBuffer.allocate(1024*3);
@@ -91,28 +80,13 @@ public class Client {
                         Object resp = Serializer.deserialize(forRead.array());
                         Respond respond = (Respond) resp;
                         System.out.println("принял ответ, вывожу");
-
-
                         System.out.println(respond.getMessage());
-
                         socketChannel.close();
-
                     }
                     else {
                         System.out.println("not connected");
                         System.exit(0);
                     }
-
-                    //ByteBuffer f = ByteBuffer.wrap(Serializer.serialize(terminal.start()));
-                    //socketChannel.write(f);
-//
-                    //f = ByteBuffer.allocate(1024*1024);
-                    //socketChannel.read(f);
-                    //ByteArrayInputStream bais = new ByteArrayInputStream(f.array());
-                    //ObjectInputStream ois = new ObjectInputStream(bais);
-                    //Respond respond = (Respond) ois.readObject();
-                    //System.out.println(respond.getMessage());
-                    //ois.close(); bais.close();
                 }
 
         } catch (Exception e) {
